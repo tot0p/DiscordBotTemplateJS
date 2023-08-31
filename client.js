f2267158cf84041d9090b2227464df3502238324
tot0p <thomas.lemaitre.78420@gmail.com>
2023-08-31 11:58:59 +0200

    rework of load commands for prepare add slash commands

---
+++ client.js
@@ -0,0 +1,91 @@
+const { Client, GatewayIntentBits,Collection } = require('discord.js');
+
+
+module.exports.client = new Client({
+	intents: [
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.MessageContent,
+		GatewayIntentBits.GuildMembers,
+	],
+});
+
+require('dotenv').config()
+const fs = require('fs');
+
+module.exports.client.login(process.env.token);
+
+module.exports.client.commands = new Collection();
+
+/**
+ * @description load Prefix Commands from ./Commandes/prefix
+ */
+module.exports.client.loadPrefixCommands = function(){
+    fs.readdir("./Commandes/prefix",(error, f) => {
+        if(error) console.log(error);
+    
+        let commandes = f.filter(f => f.split(".").pop() === "js");
+        if(commandes.length <= 0) console.log("aucune commande trouvée");
+    
+        commandes.forEach((f) => {
+            
+            let commande = require(`./Commandes/prefix/${f}`);
+            console.log(`${f} chargée`);
+            module.exports.client.commands.set(commande.help.name, commande);
+        });
+    });
+}
+
+/**
+ * @description unload Prefix Commands from ./Commandes/prefix
+ */
+module.exports.client.unloadPrefixCommands = function(){
+    fs.readdir("./Commandes/prefix",(error, f) => {
+        if(error) console.log(error);
+    
+        let commandes = f.filter(f => f.split(".").pop() === "js");
+        if(commandes.length <= 0) console.log("aucune commande trouvée");
+    
+        commandes.forEach((f) => {
+            
+            let commande = require(`./Commandes/prefix/${f}`);
+            console.log(`${f} déchargée`);
+            module.exports.client.commands.delete(commande.help.name);
+        });
+    });
+}
+
+
+/**
+ * @description reload Prefix Commands from ./Commandes/prefix
+ */
+module.exports.client.reloadPrefixCommands = function(){
+    module.exports.client.commands.clear();
+    fs.readdir("./Commandes/prefix",(error, f) => {
+        if(error) console.log(error);
+    
+        let commandes = f.filter(f => f.split(".").pop() === "js");
+        if(commandes.length <= 0) console.log("aucune commande trouvée");
+    
+        commandes.forEach((f) => {
+            
+            let commande = require(`./Commandes/prefix/${f}`);
+            console.log(`${f} rechargée`);
+            module.exports.client.commands.set(commande.help.name, commande);
+        });
+    });
+}
+
+
+// load all the events
+fs.readdir("./Events/",(error, f) => {
+    if(error) console.log(error);
+    console.log(`${f.length} events en chargement`);
+
+    f.forEach((f) => {
+        const eventsours = require(`./Events/${f}`);
+        const event = f.split(".")[0];
+
+    module.exports.client.on(event, arg => eventsours(module.exports.client, arg));
+    });
+});
--- index.js
+++ index.js
@@ -1,49 +1,5 @@
-const { Client, GatewayIntentBits,Collection } = require('discord.js');
-const client = new Client({
-	intents: [
-		GatewayIntentBits.Guilds,
-		GatewayIntentBits.GuildMessages,
-		GatewayIntentBits.MessageContent,
-		GatewayIntentBits.GuildMembers,
-	],
-});
-
-require('dotenv').config()
+const {client} = require('./client');
 
 
+client.loadPrefixCommands();
 
-const fs = require('fs');
-
-client.login(process.env.token);
-
-client.commands = new Collection();
-
-
-//load all commands
-fs.readdir("./Commandes/",(error, f) => {
-    if(error) console.log(error);
-
-    let commandes = f.filter(f => f.split(".").pop() === "js");
-    if(commandes.length <= 0) console.log("aucune commande trouvée");
-
-    commandes.forEach((f) => {
-        
-        let commande = require(`./Commandes/${f}`);
-        console.log(`${f} chargée`);
-        client.commands.set(commande.help.name, commande);
-    });
-});
-
-
-// load all the events
-fs.readdir("./Events/",(error, f) => {
-    if(error) console.log(error);
-    console.log(`${f.length} events en chargement`);
-
-    f.forEach((f) => {
-        const eventsours = require(`./Events/${f}`);
-        const event = f.split(".")[0];
-
-    client.on(event, arg => eventsours(client, arg));
-    });
-});
